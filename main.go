fddc965b7043fc365f001dcde6bcf32a1ff8da5e
Matthew Ames <git@supermatt.net>
2018-04-23 15:51:05 +0100

    Added useragent handling

--- main.go
+++ main.go
@@ -13,7 +13,14 @@ func ipHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, hp+"\n")
 }
 
+func userAgent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=UTF-8")
+	ua := r.UserAgent()
+	fmt.Fprintf(w, ua+"\n")
+}
+
 func main() {
 	http.HandleFunc("/", ipHandler)
+	http.HandleFunc("/ua", userAgent)
 	appengine.Main()
 }
